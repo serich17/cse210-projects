9cd46aff6ec78fc27489a0b5f52bbc0e98c89ed6
serich17 <samerich16@byui.edu>
2024-06-13 18:20:20 -0500

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgenerator = new Random();
+        string play = "yes";
+
+        while (play == "yes") {
+        
+        int magic = randomgenerator.Next(1,100);
+        int numGuesses = 1;
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+
+        while (guess != magic) {
+            if (guess > magic) {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic) {
+                Console.WriteLine("Higher");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            numGuesses += 1;
+
+        } 
+
+        Console.WriteLine($"You guessed it! You used {numGuesses} guesses.");
+
+
+        Console.Write("Do you want to play again?('yes' to continue) ");
+        play = Console.ReadLine();
+        }
     }
 }
