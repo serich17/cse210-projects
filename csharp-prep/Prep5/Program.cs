4b7f09f93ccdd7e57e50325eea9c2063a53994d3
serich17 <samerich16@byui.edu>
2024-06-14 10:33:54 -0500

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNum = PromptUserNumber();
+        int squared = SquareNumber(userNum);
+        DisplayResult(userName, squared);
+    }
+
+
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName() {
+        Console.Write("Please enter your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber() {
+        Console.Write("Please enter your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int num) {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int square) {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
