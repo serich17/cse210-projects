368be4fbdc556ff8542db31dcc6474305684b294
serich17 <samerich16@byui.edu>
2024-07-09 11:15:21 -0500

    Prepare activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue", 7.5);
+        Rectangle rectangle = new Rectangle("Yellow", 5, 1.5);
+        Circle circle = new Circle("Green", 5);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes) {
+            Console.WriteLine($"\nColor: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
