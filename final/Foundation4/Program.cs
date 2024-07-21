0262eefcd0326cf9de795609804ac21f7ff6ab41
serich17 <samerich16@byui.edu>
2024-07-20 21:20:13 -0500

    fix

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,15 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Activity> activities = new List<Activity>();
+
         Running running = new Running(30, 3);
-        Console.WriteLine(running.DisplaySummery());
-
         Swimming swimming = new Swimming(22, 4);
-        Console.WriteLine(swimming.DisplaySummery());
-
         Cycling cycling = new Cycling(27, 5);
-        Console.WriteLine(cycling.DisplaySummery());
+        activities.Add(running);
+        activities.Add(swimming);
+        activities.Add(cycling);
 
+
+        foreach (Activity activity in activities) {
+            Console.WriteLine(activity.DisplaySummery());
+        }
 
     }
 }
